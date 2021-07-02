118ee9abe3ae512a92c24e0a5666cb4fd8fa9c11
Ivan Ieong <ivanieong@outlook.com>
2021-07-02 18:51:58 +0800

    fill all days

--- test.py
+++ test.py
@@ -1,37 +1,39 @@
 import pandas as pd
 import numpy as np
-import datetime
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-raw_data = pd.read_csv('0700.csv', index_col = 'Date')
+def gen_data(day_range):
+  dayHigh = raw_data['High'].head(day_range)
+  dayOpen = raw_data['Open'].head(day_range)
+  output = []
+  for i in range(day_range):
+    tmp_array = []
+    for j in range(day_range):
+      if ( j >= i ):
+        up = ((dayHigh[j]/dayOpen[i]) - 1) * 100
+      else:
+        up = 0
+      tmp_array.insert(j, up)
+      continue
+    output.append(tmp_array)
+  output = np.round_(output, decimals = 2)
+  print(output)
+
+raw_data = pd.read_csv('0700.csv', index_col='Date')
+raw_data.index = pd.to_datetime(raw_data.index)
+raw_data = raw_data.resample('1D').ffill().reset_index()
+raw_data[["Day"]] = raw_data["Date"].dt.day
+raw_data[["Month"]] = raw_data["Date"].dt.month
+raw_data[["Year"]] = raw_data["Date"].dt.year
 print(raw_data)
-raw_data = raw_data.head(20)
-new_index = pd.date_range(start='2004-06-01', end='2004-06-30')
-raw_data.reindex(new_index)
-print(raw_data)
+#raw_data = raw_data.dropna()
+#df = raw_data.reset_index(drop=True)
+#raw_data = raw_data.head(10)
+print(raw_data.loc[(raw_data["Day"] == 17) & (raw_data["Month"] == 6)])
 
-#dd = pd.read_csv('0700.csv', parse_dates=['Date'])
-raw_data[["day", "month", "year"]] = raw_data["Date"].str.split("/", expand = True)
-#print(raw_data)
-raw_data = raw_data.dropna()
-df = raw_data.reset_index(drop=True)
-
-(row, num_col) = df.shape
-
-a = df['High'].head(20)
-b = df['Open'].head(20)
-
-output = []
-for i in range(len(b)):
-  tmp_array = []
-  for j in range(len(a)):
-    if ( j >= i ):
-      up = ((a[j]/b[i]) - 1) * 100
-    else:
-      up = 0
-    tmp_array.insert(j, up)
-    continue
-  output.append(tmp_array)
-output = np.round_(output, decimals = 2)
-print(len(output))
+years = np.sort(raw_data.Year.unique())
+months = np.sort(raw_data.Month.unique())
+days = np.sort(raw_data.Day.unique())
+print(years, months, days)
+#gen_data(10)
