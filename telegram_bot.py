742130d81b89c5ddc03bdef47ffef96d62f785db
Ivan Ieong <Pieong@fsm.gov.mo>
2021-07-05 17:21:16 +0800

    testing telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,24 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
-def start(bot, update):
-  bot.message.reply_text('hello, {}'.format(bot.message.from_user.first_name),
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(str(s), callback_data = '{}'.format(s)) for s in range(1, 12)]]))
+def start(update, context):
+  update.message.reply_text('輸入月份:',
+    reply_markup = InlineKeyboardMarkup(
+      [
+        [InlineKeyboardButton(str(m), callback_data = '{}'.format(m)) for m in range(1, 7)],
+        [InlineKeyboardButton(str(m), callback_data = '{}'.format(m)) for m in range(7, 13)]
+      ]
+      ))
 
-updater = Updater(token='1893410568:AAHjzrw8EfE3cAv1tjWZ2okgLvmLals3gN8', request_kwargs={'proxy_url': 'http://192.168.61.211:80'})
+def answer(update, context):
+    bm = update.callback_query.data
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
+def main():
+  updater = Updater(token='1893410568:AAHjzrw8EfE3cAv1tjWZ2okgLvmLals3gN8', request_kwargs={'proxy_url': 'http://192.168.61.211:80'})
+  updater.dispatcher.add_handler(CommandHandler('start', start))
+  updater.dispatcher.add_handler(CallbackQueryHandler(answer))
+  updater.start_polling()
+  updater.idle()
 
-updater.start_polling()
-updater.idle()
+if __name__ == "__main__":
+    main()
